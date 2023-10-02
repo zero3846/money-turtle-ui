272ec0bb9402359f406da09f3645b0e5b5f99d15
Miguel Borromeo <iggy47@gmail.com>
2023-10-01 20:49:00 -0500

    Designed login UI

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
+import * as client from './client.js'
+
 const appElem = document.querySelector('.app');
 appElem.innerHTML = `<h1>Money Turtle</h1>`;
+
+if (client.getCurrentUser()) {
+    console.log(`Logged in as ${client.getCurrentUser()}`);
+} else {
+    console.log(`Logged out`);
+
+    appElem.classList = 'app login';
+    appElem.innerHTML = `
+        <div class="flex column">
+            <h1>Money Turtle</h1>
+            <form class="login-form">
+                <input id="username" placeholder="Username" type="text">
+                <input id="password" placeholder="Password" type="password">
+                <input class="button" type="submit" value="Login">
+            </form>
+        </div>
+    `;
+}
---
+++ js/client.js
@@ -0,0 +1,3 @@
+export function getCurrentUser() {
+    return null;
+}

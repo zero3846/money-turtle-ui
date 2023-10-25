1c2ae7816fbfd1d52894bc0d86f8307519171836
Miguel Borromeo <iggy47@gmail.com>
2023-10-25 00:44:44 -0500

    Added interface for transactions

---
+++ js/data.js
@@ -0,0 +1,31 @@
+export function listTransactions(criteria) {
+    return { error: "Not yet implemented" };
+}
+
+export function postTransactions(data) {
+    return { error: "Not yet implemented" };
+}
+
+export function updateTransactions(criteria, data) {
+    return { error: "Not yet implemented" };
+}
+
+export function deleteTransactions(criteria) {
+    return { error: "Not yet implemented" };
+}
+
+export function listTransactionGroups(criteria) {
+    return { error: "Not yet implemented" };
+}
+
+export function postTransactionGroups(data) {
+    return { error: "Not yet implemented" };
+}
+
+export function updateTransactionGroups(criteria, changes) {
+    return { error: "Not yet implemented" };
+}
+
+export function deleteTransactionGroups(criteria) {
+    return { error: "Not yet implemented" };
+}

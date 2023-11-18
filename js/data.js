5cf4fb31a0be46935720d107e6223707e2d80f86
Miguel Borromeo <iggy47@gmail.com>
2023-11-18 16:55:09 -0600

    Added ledgers.js to the data model.

--- js/data.js
+++ js/data.js
@@ -1,2 +1,6 @@
+// TODO: Remove these when you get the chance
 export * from './data/categories.js'
 export * from './data/transactions.js'
+
+// New modules based on TODO.md
+export * from './data/ledgers.js'

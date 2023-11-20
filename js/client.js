5241c72510e3b14d083efb6e7886af0a787bead8
Miguel Borromeo <iggy47@gmail.com>
2023-11-19 22:13:14 -0600

    Expanded example data

--- js/client.js
+++ js/client.js
@@ -1,17 +1,33 @@
 import * as data from './data.js'
 
+const currency = 'USD';
+const ledgerId = data.createLedger(getCurrentUser(), 'Default Ledger');
+const incomeId = data.createAccount(ledgerId, 'Income Earned', 'Income', 'My total income earned');
+
 export function getCurrentUser() {
     return 'Turtle';
 }
 
 export function loadUserData() {
-    const ledgerId = data.createLedger(getCurrentUser(), 'Example Ledger');
-
     const checkingId = data.createAccount(ledgerId, 'First Example Bank', 'Checking', 'My checking account');
-    const incomeId = data.createAccount(ledgerId, 'Income Earned', 'Income', 'My total income earned');
 
-    data.createLedgerEntry(ledgerId, checkingId, incomeId, 10000, 'USD', new Date());
+    const emergenciesId = data.createAccount(ledgerId, 'Emergency Fund', 'Fund', 'Fund for unexpected emergencies');
+    const groceriesId = data.createAccount(ledgerId, 'Groceries', 'Fund', 'Groceries spending fund');
+    const rentId = data.createAccount(ledgerId, 'Rent', 'Fund', 'Fund for paying rent');
+
+    earn(checkingId, 10000, new Date());
+    fund(emergenciesId, checkingId, 3000, new Date());
+    fund(groceriesId, checkingId, 3000, new Date());
+    fund(rentId, checkingId, 4000, new Date());
 
     const entries = data.getLedgerEntries();
     console.log(entries);
+}
+
+function earn(debited, amount, date) {
+    data.createLedgerEntry(ledgerId, debited, incomeId, amount, currency, date);
+}
+
+function fund(account, from, amount, date) {
+    data.createLedgerEntry(ledgerId, account, from, amount, currency, date);
 }
